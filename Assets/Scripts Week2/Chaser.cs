9e55e764ed10bf7b0c3e1c7fa6b928a0668b1d0d
Val Burnett <115892294+JamieBurnett@users.noreply.github.com>
2025-07-08 17:41:23 -0400

    Altered following to be based on speed rather than snapping
    note that the speed of the object right now will be altered by proximity to the mouse position

--- Assets/Scripts Week2/Chaser.cs
+++ Assets/Scripts Week2/Chaser.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class Chaser : MonoBehaviour
 {
     public Vector3 newPosition;
+    public Vector3 moveDir;
+    public Camera gameCamera;
+
+    //Excercise 1 stuff
+    public float speed;
+    private Vector3 targetPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = newPosition;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector3 mouseWorldPos = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+       // mouseWorldPos.z = 0f;
+       // transform.position = mouseWorldPos;
+    }
+
+    private void FixedUpdate()
+    {
+        //I don't believe fixedupdate has been brought up at this point
+        //but it's better for motion/physics so...
+        Vector3 mouseWorldPos = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0f;
+
+        moveDir = (mouseWorldPos - transform.position )*speed;
+        transform.position += moveDir;
+
+
     }
 }
