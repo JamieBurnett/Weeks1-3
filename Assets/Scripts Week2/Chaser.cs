0c2551f43b2e8d36aad7168901253b632aed9023
Val Burnett <115892294+JamieBurnett@users.noreply.github.com>
2025-07-08 17:53:33 -0400

    pt1: square now moves to cursor on click

--- Assets/Scripts Week2/Chaser.cs
+++ Assets/Scripts Week2/Chaser.cs
@@ -12,30 +12,32 @@ public class Chaser : MonoBehaviour
     public float speed;
     private Vector3 targetPos;
 
+    //Excercise 2 stuff
+    private SpriteRenderer mySprite;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        mySprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 mouseWorldPos = gameCamera.ScreenToWorldPoint(Input.mousePosition);
-       // mouseWorldPos.z = 0f;
-       // transform.position = mouseWorldPos;
+        Vector3 mouseWorldPos = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0f;
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = mouseWorldPos;
+            //moveDir = (mouseWorldPos - transform.position) * speed;
+        }
     }
 
     private void FixedUpdate()
     {
         //I don't believe fixedupdate has been brought up at this point
-        //but it's better for motion/physics so...
-        Vector3 mouseWorldPos = gameCamera.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPos.z = 0f;
-
-        moveDir = (mouseWorldPos - transform.position )*speed;
-        transform.position += moveDir;
-
-
+        //but it's better for motion/physics consistency, apologies if this isn't allowed.
+        
+        //transform.position += moveDir; (I will use this in the next step)
     }
 }
